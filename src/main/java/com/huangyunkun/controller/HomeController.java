465c52f50a23c57231e853cc7798449b7ba85b38
Huang YunKun <htynkn@gmail.com>
2017-04-02 18:25:16 +0800

    add upload

---
+++ src/main/java/com/huangyunkun/controller/HomeController.java
@@ -0,0 +1,41 @@
+package com.huangyunkun.controller;
+
+import com.huangyunkun.service.TorrentService;
+import net.glxn.qrgen.javase.QRCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+@Controller
+public class HomeController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private TorrentService torrentService;
+
+    @GetMapping("/")
+    public String showHomePage() {
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String transformFileToQR(@RequestParam("file") MultipartFile file) throws IOException, NoSuchAlgorithmException {
+        File torrent = File.createTempFile("torrent", ".torrent");
+        file.transferTo(torrent);
+        logger.info("Saving file to {}", torrent.getPath());
+        String link = torrentService.getLink(torrent);
+        logger.info("Torrent link is {}", link);
+        File QR = QRCode.from(link).file();
+        logger.info("QR file path: " + QR.getAbsolutePath());
+        return "home";
+    }
+}
